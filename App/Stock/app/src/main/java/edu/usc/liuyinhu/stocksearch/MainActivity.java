9ad038dd2fdbd5b36125d582aac16080312742a5
louiehuang <1015110020@qq.com>
2017-11-23 21:43:03 -0800

    tab

--- App/Stock/app/src/main/java/edu/usc/liuyinhu/stocksearch/MainActivity.java
+++ App/Stock/app/src/main/java/edu/usc/liuyinhu/stocksearch/MainActivity.java
@@ -1,13 +1,40 @@
 package edu.usc.liuyinhu.stocksearch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        configureSwitchButton();
+
     }
+
+    private void configureSwitchButton() {
+        Button switchBtn = (Button) findViewById(R.id.switch_btn);
+        switchBtn.setOnClickListener(new View.OnClickListener(){
+            private static final String TAG = "MainActivity";
+
+            @Override
+            public void onClick(View v) {
+                {
+                    Log.v(TAG, "Clicked");
+                }
+                startActivity(new Intent(MainActivity.this, StockDetailsActivity.class));
+            }
+        });
+    }
+
+
 }
